1839a81d189aad641ab3cb4884b5f8926b5939bb
Toofan <Toofan534@gmail.com>
2022-08-19 20:27:35 +0430

    MusicVue has been updated

--- src/utils/fontawesome.js
+++
@@ -1,7 +0,0 @@
-import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
-import { library } from '@fortawesome/fontawesome-svg-core'
-import { faBell, faCheck, faExclamation, faInfo, faTimes } from '@fortawesome/free-solid-svg-icons'
-
-library.add(faBell, faCheck, faExclamation, faInfo, faTimes)
-
-export default FontAwesomeIcon
